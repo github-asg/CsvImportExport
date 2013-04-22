19a26016a7568ebaf6c8fc22611d86b9307407bc
Aakash Goyal <aakash140489@gmail.com>
2013-04-22 18:26:09 +0530

    Initial commit

---
+++ CsvExportImport/csv/Test.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+import java.io.IOException;
+
+public class Test
+{
+public static void main(String[] ops) throws IOException
+{
+try
+{
+  CsvUnitTest csv = new CsvUnitTest();
+  csv.test();
+}
+catch(Exception e)
+{
+}
+}
+}
---
+++ CsvExportImport/hbase/HBaseImportExport.java
@@ -0,0 +1,16 @@
+package com.cl.hbase;
+
+public class HBaseImportExport {
+	
+	public String DEFAULT_COLUMN_FAMILY = "c1";
+	
+	public class HBaseCol {
+		String family;
+		String qualifier;
+		HBaseCol(String family,String qualifier) {
+			this.family = family;
+			this.qualifier = qualifier;
+		}
+	}
+
+}
